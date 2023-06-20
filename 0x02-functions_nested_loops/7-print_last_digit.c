1490226d84403831b81b90b6e317fe4c62a37bb9
wahito-p <wahitoprecious@gmail.com>
2023-06-20 14:25:34 -0700

    done

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,8 @@
+include "main.h"
+
+/**
+ * _abs - checks if number is positive or negative 
+ * _abs - checks if number is positive or negative
+ * @n: integer to be tested
+ * Return: returns value of abs(n)
+ */
