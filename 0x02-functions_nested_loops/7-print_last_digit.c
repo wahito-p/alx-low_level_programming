17faa34b013f840aa31ae9eff176ff7240e2648e
wahito-p <wahitoprecious@gmail.com>
2023-06-20 14:28:58 -0700

    done

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,8 +1,24 @@
-include "main.h"
+#include "main.h"
 
 /**
- * _abs - checks if number is positive or negative 
- * _abs - checks if number is positive or negative
- * @n: integer to be tested
- * Return: returns value of abs(n)
+ * print_last_digit - prints the last digit of a number
+ * @n: the int to extract the last digit from
+ * Return: value of the last digit
  */
+int print_last_digit(int n)
+{
+	int a;
+
+	if (n < 0)
+	n = -n;
+
+	a = n % 10;
+
+	if (a < 0)
+		a = -a;
+
+	_putchar(a + '0');
+
+	return (a);
+}
+
